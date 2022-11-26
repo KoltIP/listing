ad8d8c5b8ce479dcbded2acab6f7857318ef598a
kolt <ilya.kolt@gmail.com>
2022-11-26 23:44:11 +0300

    initialize

---
+++ src/components/Listing.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Listing = (props) => {
+    console.log(props);
+    const products = props.products;
+
+    return(   
+        <div>
+            Хорошо
+        </div>
+    )    
+}
+export default Listing;
